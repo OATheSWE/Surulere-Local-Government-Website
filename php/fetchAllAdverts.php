9f61244f97bbe4bb164d06cac161e17610291385
OATheSWE <osborneosas12@gmail.com>
2024-10-21 16:15:02 +0100

    Done with fetch all gallery & advert

---
+++ php/fetchAllAdverts.php
@@ -0,0 +1,32 @@
+<?php
+// Include the database connection
+include 'db.php';
+
+// Fetch all adverts from the adverts table
+$fetchQuery = "SELECT advert_id, advert_data FROM adverts";
+$fetchResult = pg_query($connection, $fetchQuery);
+
+// Check if the query execution was successful
+if (!$fetchResult) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Failed to fetch adverts: " . pg_last_error()
+    ]);
+    exit;
+}
+
+// Fetch all rows as an associative array
+$adverts = [];
+while ($row = pg_fetch_assoc($fetchResult)) {
+    $adverts[] = [
+        "advert_id" => $row['advert_id'],
+        "advert_data" => json_decode($row['advert_data'], true)
+    ];
+}
+
+// Return the list of adverts in JSON format
+echo json_encode([
+    "status" => "success",
+    "data" => $adverts
+]);
+?>
---
+++ php/fetchAllGallery.php
@@ -0,0 +1,32 @@
+<?php
+// Include the database connection
+include 'db.php';
+
+// Fetch all gallery items from the gallery table
+$fetchQuery = "SELECT gallery_id, gallery_data FROM gallery";
+$fetchResult = pg_query($connection, $fetchQuery);
+
+// Check if the query execution was successful
+if (!$fetchResult) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Failed to fetch gallery items: " . pg_last_error()
+    ]);
+    exit;
+}
+
+// Fetch all rows as an associative array
+$galleryItems = [];
+while ($row = pg_fetch_assoc($fetchResult)) {
+    $galleryItems[] = [
+        "gallery_id" => $row['gallery_id'],
+        "gallery_data" => json_decode($row['gallery_data'], true)
+    ];
+}
+
+// Return the list of gallery items in JSON format
+echo json_encode([
+    "status" => "success",
+    "data" => $galleryItems
+]);
+?>

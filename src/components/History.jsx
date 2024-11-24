5bc76981599f8a6b633225c12e3926d97e15c703
OATheSWE <osborneosas12@gmail.com>
2024-11-24 21:03:24 +0100

    Done with history page

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -42,77 +42,134 @@ export default function History() {
     config: { mass: 1, tension: 80, friction: 26 },
   });
 
+
+
   return (
     <section ref={ref} className={`w-full py-10 ${styles.body} bg-default`}>
-      <Grid gutter={90} className={`font-sans mt-12`}>
+      <Grid gutter={90} className="font-sans mt-12">
         <Grid.Col span={{ base: 12 }}>
-          <animated.div style={rightColAnimation} className={`text-black`}>
-            <Text className="my-2 text-center">
-              Founded in 2023, Krowdkontrollers aims to establish a versatile
-              ticket management platform, empowering event planners to sell
-              tickets for diverse occasions, from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globally.
-              Founded in 2023, Krowdkontrollers aims to establish a versatile
-              ticket management platform, empowering event planners to sell
-              tickets for diverse occasions, from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globally.
-            </Text>
-          </animated.div>
+          <div className="text-black">
+            <div className="my-2 text-center">
+              <strong>Pre-Colonial Era</strong> 
+              <ul>
+                <li>
+                  Surulere was originally inhabited by the Awori people, a
+                  subgroup of the Yoruba ethnic group.
+                </li>
+                <li>The area was a major farming and fishing community.</li>
+              </ul>
+              <br />
+              <strong>Colonial Era (1861-1960)</strong>
+              <ul>
+                <li>
+                  1861: British colonialists arrived in Lagos and established
+                  the city as a colony.
+                </li>
+                <li>
+                  Late 19th century: Surulere became a popular residential area
+                  for Africans and Europeans.
+                </li>
+                <li>
+                  1914: The Nigerian Railway Corporation built a railway line
+                  through Surulere, boosting economic growth.
+                </li>
+              </ul>
+            </div>
+          </div>
         </Grid.Col>
       </Grid>
-      <Grid gutter={90} className={`font-sans mt-4`}>
-        <Grid.Col span={{ base: 12, md: 6.7 }}>
-          <animated.div style={rightColAnimation2} className={`text-black`}>
-            <Text className="my-2 text-center">
-              Founded in 2023, Krowdkontrollers aims to establish a versatile
-              ticket management platform, empowering event planners to sell
-              tickets for diverse occasions, from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globally. from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globallyh
-            </Text>
 
-          </animated.div>
+      <Grid gutter={90} className="font-sans mt-4">
+        <Grid.Col span={{ base: 12, md: 6.7 }}>
+          <div className="text-black">
+            <div className="my-2 text-center">
+              <strong>Post-Colonial Era (1960-1980)</strong>
+              <ul>
+                <li>1960: Nigeria gained independence from Britain.</li>
+                <li>
+                  1960s-1970s: Surulere experienced rapid urbanization and
+                  growth, becoming a commercial hub.
+                </li>
+                <li>
+                  1970s: The National Stadium (now Moshood Abiola Stadium) was
+                  built, hosting international football matches.
+                </li>
+              </ul>
+              <br />
+              <strong>Modern Era (1980-Present)</strong>
+              <ul>
+                <li>
+                  1980s: Surulere became a hub for entertainment, with the
+                  establishment of the National Arts Theatre.
+                </li>
+                <li>
+                  1990s: The area experienced infrastructure development,
+                  including road construction and shopping centers.
+                </li>
+                <li>
+                  2000s: Urban renewal efforts improved housing, transportation,
+                  and amenities.
+                </li>
+                <li>
+                  Present day: Surulere remains a vibrant, cosmopolitan area
+                  with a mix of residential, commercial, and recreational
+                  activities.
+                </li>
+              </ul>
+            </div>
+          </div>
         </Grid.Col>
         <Grid.Col span={{ base: 12, md: 5.3 }} className="flex">
-          <animated.div
-            style={leftColAnimation}
+          <div
             className="flex items-stretch max-lg:mx-auto"
           >
             <Image
               src={ImageCollection.hero}
-              className={`w-full object-cover object-top rounded-xl min-h-[200px
-                ]`}
+              className="w-full object-cover object-top rounded-xl min-h-[200px]"
               alt="About Image"
             />
-          </animated.div>
+          </div>
         </Grid.Col>
       </Grid>
-      <Grid gutter={90} className={`font-sans mt-4`}>
+
+      <Grid gutter={90} className="font-sans mt-4">
         <Grid.Col span={{ base: 12 }}>
-          <animated.div style={rightColAnimation3} className={`text-black`}>
-            <Text className="my-2 text-center">
-              Founded in 2023, Krowdkontrollers aims to establish a versatile
-              ticket management platform, empowering event planners to sell
-              tickets for diverse occasions, from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globally.
-              Founded in 2023, Krowdkontrollers aims to establish a versatile
-              ticket management platform, empowering event planners to sell
-              tickets for diverse occasions, from intimate gatherings to
-              sprawling music festivals. Our platform offers organizers
-              comprehensive control and oversight, setting a new benchmark for
-              event management both nationally and globally.
-            </Text>
-          </animated.div>
+          <div className="text-black">
+            <div className="my-2 text-center">
+              <strong>Notable Landmarks</strong>
+              <ul>
+                <li>National Stadium (Moshood Abiola Stadium)</li>
+                <li>National Arts Theatre</li>
+                <li>Surulere Local Government Secretariat</li>
+                <li>Adeniran Ogunsanya Shopping Centre</li>
+                <li>Teslim Balogun Stadium</li>
+              </ul>
+              <br />
+              <strong>Cultural Significance</strong>
+              <ul>
+                <li>Home to the famous Surulere Carnival</li>
+                <li>
+                  Hosts various cultural festivals, including the Awori Festival
+                </li>
+                <li>Hub for Nigerian music and arts</li>
+              </ul>
+              <br />
+              <strong>Challenges</strong>
+              <ul>
+                <li>Traffic congestion</li>
+                <li>Overcrowding</li>
+                <li>Infrastructure decay</li>
+                <li>Environmental concerns</li>
+              </ul>
+              <br />
+              <strong>Future Developments</strong>
+              <ul>
+                <li>Proposed light rail system</li>
+                <li>Renewal of the National Stadium</li>
+                <li>Expansion of commercial and residential areas</li>
+              </ul>
+            </div>
+          </div>
         </Grid.Col>
       </Grid>
     </section>

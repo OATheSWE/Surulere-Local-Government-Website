2a8650711f1098f39f6d3b77d37495c7635c09e2
OATheSWE <osborneosas12@gmail.com>
2024-11-24 22:26:57 +0100

    Done for the gallery page

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -2,14 +2,19 @@ import { Grid, Image } from "@mantine/core";
 import { useTrail, animated } from "@react-spring/web";
 import { useInView } from "react-intersection-observer";
 import { galleryImages, styles } from "../data";
+import { api } from "../api";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const GalleryPage = () => {
+  const [gallery, setGallery] = useState([]);
+
   const [ref, inView] = useInView({
     threshold: 0.4,
     triggerOnce: true,
   });
 
-  const trail = useTrail(galleryImages.length, {
+  const trail = useTrail(gallery.length, {
     opacity: inView ? 1 : 0,
     transform: inView ? "translateX(0)" : "translateX(-50%)",
     filter: inView ? "blur(0)" : "blur(4px)",
@@ -17,11 +22,31 @@ const GalleryPage = () => {
     delay: 300, // Adjust this delay based on your preference
   });
 
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+
+  const fetchGallery = async () => {
+    try {
+      const response = await axios.get(api.fetchAllGallery, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      if (response.data.status === "success") {
+        setGallery(response.data.gallery);
+      }
+    } catch (error) {
+      console.error("Failed to fetch gallery:", error);
+    }
+  };
+
   const allImages = trail.map((style, index) => (
     <Grid.Col ref={ref} span={{ base: 12, xs: 6, sm: 4, md: 3 }} key={index} className="mt-8">
       <animated.div style={style} className={`w-full h-[250px] overflow-hidden rounded-xl`}>
         <Image
-          src={galleryImages[index].src}
+          src={gallery[index]?.gallery_data?.file_path}
           className={`w-full h-full object-cover object-top rounded-xl transition duration-300 hover:scale-110`}
           alt="Gallery Image"
           loading="lazy"

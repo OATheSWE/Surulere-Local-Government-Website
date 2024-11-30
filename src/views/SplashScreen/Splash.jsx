9fd1c9b2dbd2272c64f56ac859c5a8353fe3b784
OATheSWE <osborneosas12@gmail.com>
2024-11-30 10:17:57 +0000

    Done for now

--- src/views/SplashScreen/Splash.jsx
+++ src/views/SplashScreen/Splash.jsx
@@ -8,7 +8,7 @@ const Splash = () => {
     <div className={`w-full bg-primary h-screen flex flex-col justify-center items-center ${styles.body}`}>
       {/* Animated Text */}
       <Text className="typewriter text-[30px] font-bold text-center text-[#d3f7c1] font-sans mt-6 radiant">
-        WELCOME TO SURULERE LOCAL GOVERNMENT
+        WELCOME TO<br />SURULERE<br />LOCAL<br />GOVERNMENT
       </Text>
     </div>
   );
